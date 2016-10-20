887ffb73a099b59951fed8550f3c480c6cf9769f
Marcel Kocks <marcel.kocks@hotmail.de>
2016-10-20 14:51:47 +0200

    logik

--- 3.1.1 Wahrheitstafeln/main.c
+++ 3.1.1 Wahrheitstafeln/main.c
@@ -3,13 +3,18 @@
 
 void main() {
 
-	printf(" A | B | C | D || log1");
+	printf("| A | B | C | D || 1 || 2 || 3 || 4 ||\n");
+	printf("|---|---|---|---||---||---||---||---||\n");
 
 	for (short a = 0; a < 2; a++) {
 		for (short b = 0; b < 2; b++) {
 			for (short c = 0; c < 2; c++) {
 				for (short d = 0; d < 2; d++) {
-					printf(" %d | %d | %d | %d || ");
+					short e1 = !(a&&b)||(c||d);
+					short e2 = (!((!(a&&b))||c))&&d;
+					short e3 = 0;
+					short e4 = 0;
+					printf("| %1d | %1d | %1d | %1d || %1d || %1d || %1d || %1d ||\n",a,b,c,d,e1,e2,e3,e4);
 				}
 			}
 		}
