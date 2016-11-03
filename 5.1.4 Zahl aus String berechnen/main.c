4872ce28cae3b81dd6837b08a5c534b7b0915f70
Marcel Kocks <marcel.kocks@hotmail.de>
2016-11-03 12:05:32 +0100

    Mittag

---
+++ 5.1.4 Zahl aus String berechnen/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+int StrToInt(char* str) {
+	int res = 0;
+
+	for (; *str != 0; str++) {
+		res = res * 10 + (*str - '0');
+	}
+
+	return res;
+}
+
+void main() {
+	char input[100];
+
+	printf("Zahl eingeben: ");
+	scanf_s("%s", input, 100);
+
+	printf("Eingabe: %d\n", StrToInt(input));
+}
