03d3548e0e0c99f99aa8b21f39f14f4491ae4006
Marcel-Kocks <marcel.kocks@hotmail.de>
2017-05-04 15:02:24 +0200

    Add files via upload

---
+++ Betriebssysteme/Uebung_2/u2_16/build/CMakeFiles/u2_16.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/u2_16.dir/main.o"
+  "u2_16.pdb"
+  "u2_16"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/u2_16.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Betriebssysteme/Uebung_2/u2_16/main.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <pthread.h>
+
+struct eqaul{
+    float op1;
+    float op2;
+    char op;
+    float erg;
+};
+
+void* calc(void * eq){
+    struct eqaul* eqq = (struct eqaul*) eq;
+    switch(eqq->op){
+        case '+' : eqq->erg=eqq->op1 + eqq->op2;break;
+        case '-' : eqq->erg=eqq->op1 - eqq->op2;break;
+        case '*' : eqq->erg=eqq->op1 * eqq->op2;break;
+        case '/' : eqq->erg=eqq->op1 / eqq->op2;break;
+    }
+    
+}
+
+int main(int argc, char **argv) {
+    
+    struct eqaul arr_eq[4];
+    arr_eq[0] = (struct eqaul){12,24,'+'};
+    arr_eq[1] = (struct eqaul){13,24,'+'};
+    arr_eq[2] = (struct eqaul){14,24,'+'};
+    arr_eq[3] = (struct eqaul){15,24,'+'};
+    
+    pthread_t threads[4];
+    
+    int i;
+    for(i=0;i<4;i++){
+        pthread_create(&threads[i],NULL,calc,arr_eq+i);
+    }
+    
+    for(i=0;i<4;i++){
+        float** erg;
+        pthread_join(threads[i],(void**)erg);
+        printf("OUT: %f\n",**erg);
+    }
+    
+    /*
+    for(i=0;i<4;i++){
+        printf("OUT: %f\n",arr_eq[i].erg);
+    }*/
+    
+    
+    printf("Hello Lucas\n");
+    //sleep(5);
+    return 0;
+}
